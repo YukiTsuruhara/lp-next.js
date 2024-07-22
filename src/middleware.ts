b6465562cb60e2a3bcc000375191140692ce082f
YukiTsuruhara <y.tsuruhara@leihauoli.com>
2024-07-22 20:50:41 +0900

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/:path*"],
+};
+
+export default function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get("authorization");
+  if (process.env.ENV === "prodction" || "local") return;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, password] = atob(authValue).split(":");
+
+    if (
+      user === process.env.BASIC_USER &&
+      password === process.env.BASIC_PASS
+    ) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse("Unauthorized.", {
+    status: 401,
+    headers: {
+      "WWW-authenticate": 'Basic realm="Secure Area"',
+    },
+  });
+}
